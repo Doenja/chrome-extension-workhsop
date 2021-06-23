1499d153749265faa8c17fb4b42e652b8cc004e3
Doenja <doenjamaris@gmail.com>
2021-06-23 17:24:31 +0200

    Clean up

--- popup.js
+++ popup.js
@@ -1,15 +1,6 @@
 const input = document.querySelector('input');
 
-if(input) {
-    input.addEventListener('input', (e) => {
-        const color = e.target.value;
-        
-        if(color) {
-            sendColorSelectedMessage(color);
-        }
-    })
-}
-
-function sendColorSelectedMessage(color) {
-    chrome.runtime.sendMessage({ message: 'color selected', value: color});
-}
+input.addEventListener('input', (e) => {
+    const color = e.target.value;
+    chrome.runtime.sendMessage({ message: 'color input', value: color});
+})
--- serviceworker.js
+++ serviceworker.js
@@ -1,41 +1,35 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if(request.message && request.message === 'color selected') {
+    if(request.message && request.message === 'color input') {
         saveColor(request.value);
-        sendColorSaved(request.value);
+        sendColor(request.value);
     }
 });
 
-chrome.runtime.onInstalled.addListener(() => {
-    saveColor('#779');
-})
-
 chrome.tabs.onUpdated.addListener((tabId, updateInfo, tab) => {
     if(updateInfo.status === 'complete' && /^http/.test(tab.url)) {
-        getSavedColor();
+        sendSavedColor();
     }
 })
 
-function saveColor(color) {
-    chrome.storage.local.set({color: color}, function() {
-        console.log('Value is set to ' + color);
-      }); 
-}
-
-function getSavedColor() {
-    chrome.storage.local.get(['color'], function(result) {
-        chrome.storage.local.get(['color'], function(result) {
-            console.log('Value currently is ' + result.color);
-        }); 
-
-        sendColorSaved(result.color);
-    });
-}
-
-async function sendColorSaved(color) {
+async function sendColor(color) {
     const tab = await chrome.tabs.query({ active: true});
 
     if(tab[0] && tab[0].id) {
         chrome.tabs.sendMessage(tab[0].id, { message: 'color saved', value: color})
     }
 }
+
+function saveColor(color) {
+    chrome.storage.local.set({color: color}); 
+}
+
+function sendSavedColor() {
+    chrome.storage.local.get(['color'], (result) => {
+        if(result.color) {
+            sendColor(result.color);
+        }
+    });
+}
+
+
 
